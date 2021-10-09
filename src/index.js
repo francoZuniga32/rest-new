ed123cdf72839b10b2c41514583fabbdaa199889
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2021-10-09 11:17:50 -0300

    feat: configuracion inidial solo queda diseñar el sistsma del frontend

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(process.env.PORT, ()=>{
+
+});
